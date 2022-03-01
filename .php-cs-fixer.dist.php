1aea51f00316ac5ebce148263fe5219bfdf50bac
Leo Unglaub <leo@unglaub.at>
2022-03-01 04:15:25 +0100

    A fresh .php-cs-fixer.dist.php file
    This also serves as a template
    
    Signed-off-by: Leo Unglaub <leo@unglaub.at>

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * The default config for all our projects. No forther config needed.
+ *
+ * @version 0.2.0
+ * @see https://code.foxship.com/foxship/server
+ */
+
+return new LeoUnglaub\LaravelBaselineDev\PhpCsFixer\Config();
